9e16cf253a3e2500c4fbbfc46015d979e978bd8b
Nassredean Nasseri <nassredean@gmail.com>
2022-07-29 15:50:41 -0400

    Death animations

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -1,10 +1,14 @@
 extends KinematicBody2D
 
+# Movement
 var speed : int = 100
 var jump_speed : int = 220
 var gravity : int = 660
 var velocity = Vector2.ZERO
 var initial_position = Vector2.ZERO
+
+# Character state
+var respawning = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,39 +19,40 @@ func _physics_process(delta):
 	process_collisions()
 
 func get_input(delta):
-	velocity.x = 0
-	if Input.is_action_pressed("move_right"):
-		$Sprite.set_flip_h(false)
-		velocity.x += speed
-		if is_on_floor():
-			$AnimationPlayer.play("Walk_Right")
-		
-	if Input.is_action_pressed("move_left"):
-		$Sprite.set_flip_h(true)
-		velocity.x -= speed
-		if is_on_floor():
-			$AnimationPlayer.play("Walk_Right")
-
-	if Input.is_action_pressed("jump"):
-		if (is_on_floor()):
-			velocity.y -= jump_speed
-			# Stop the running/idle animations when jumping
-			$AnimationPlayer.stop()
-			$AnimationPlayer.seek(2, true)
+	if !respawning:
+		velocity.x = 0
+		if Input.is_action_pressed("move_right"):
+			$RunIdleSwipeSprite.set_flip_h(false)
+			velocity.x += speed
+			if is_on_floor():
+				$AnimationPlayer.play("Walk_Right")
 			
-	# Play idle animation when not moving		
-	if velocity.x == 0 and velocity.y == 0:
-		$AnimationPlayer.play("Idle")
-		
-	#gravity
-	velocity.y += gravity * delta
-	
-	# apply movement
-	velocity = move_and_slide(velocity, Vector2.UP)
+		if Input.is_action_pressed("move_left"):
+			$RunIdleSwipeSprite.set_flip_h(true)
+			velocity.x -= speed
+			if is_on_floor():
+				$AnimationPlayer.play("Walk_Right")
+
+		if Input.is_action_pressed("jump"):
+			if (is_on_floor()):
+				velocity.y -= jump_speed
+				# Stop the running/idle animations when jumping
+				$AnimationPlayer.stop()
+				$AnimationPlayer.seek(2, true)
+				
+		# Play idle animation when not moving		
+		if velocity.x == 0 and velocity.y == 0:
+			$AnimationPlayer.play("Idle")
+			
+		#gravity
+		velocity.y += gravity * delta
+
+		# apply movement
+		velocity = move_and_slide(velocity, Vector2.UP)
 
 func respawn():
 	position = initial_position
-	pass
+	respawning = false
 
 func process_collisions():
 	# get collisioins
@@ -55,5 +60,10 @@ func process_collisions():
 		var col = get_slide_collision(i)
 		if col.collider.is_in_group("spike"):
 			get_node("/root/Main/HealthBar").do_damage(1)
-			respawn()
+			respawning = true
+			$AnimationPlayer.play("Death")
 			break
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "Death":
+		respawn()
